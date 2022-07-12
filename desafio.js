48e91ba4e072e567b4a46ae1a6e987dc7453a6b0
Gonzalo Celan <gonzaloezequielcelan@hotmail.com>
2022-07-12 13:22:49 -0300

    desafio-array

--- desafio.js
+++ desafio.js
@@ -2,17 +2,34 @@
 
 
     
-   
-
-    let ingresar = prompt("¡Bienvenidos!. Desea realizar una compra: ingrese Si o No.");
-
-    while( ingresar != "No"){
 
 
+
+    
+    
+    let datos_compra = [];
+    
+    let ingresar = prompt("¡Bienvenidos!. Desea realizar una compra: ingrese Si o No.").toLowerCase();
+
+    while( ingresar != "no"){
+
+    // precios de productos
     let Buzo = 7500;
     let Campera = 9500;
+    let Remera = 3000;
+    let Chaleco = 5500;
 
-        let dato_uno = prompt("Seleccione el producto; Buzo, Campera.");
+    class producto{
+        constructor(dato_uno, dato_dos, dato_tres,){
+            
+            this.dato_uno = dato_uno;
+            this.dato_dos = dato_dos;
+            this.dato_tres = dato_tres;
+            
+        }
+    }
+
+        let dato_uno = prompt("Seleccione el producto; Buzo, Campera, Remera, Chaleco.");
     console.log("Producto seleccionado:", dato_uno);
     
     let dato_dos = prompt("Seleccione talle; S, M, L, XL.");
@@ -39,10 +56,31 @@
         
     
         }
+
+        else if( dato_uno == "Remera"){
+            let suma = Remera * parseInt(dato_tres);
+            console.log("Valor total:", suma);
+            alert("El valor total de la compra es:" + " " + "$" +suma);
+        
+    
+        }
+
+        else if( dato_uno == "Chaleco"){
+            let suma = Chaleco * parseInt(dato_tres);
+            console.log("Valor total:", suma);
+            alert("El valor total de la compra es:" + " " + "$" +suma);
+        
+    
+        }
     }
     
     suma_producto();
-    
+
+    let compra_usuario = new producto (dato_uno, dato_dos, dato_tres,);
+
+    datos_compra.push(compra_usuario);
+
+
     alert("¡Muchas gracias por su compra!");
 
     ingresar = prompt("Desea seguir comprando; Si o No.")
@@ -50,6 +88,8 @@
 
 
     }
+
+    console.log("El usuario realizo la siguiente compra:", datos_compra);
 
 
 
